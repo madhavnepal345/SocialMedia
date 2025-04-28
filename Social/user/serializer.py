d0084087248456a616adf71073fabc979cbf72e5
madhav <madhavnepal916@gmail.com>
2025-04-28 18:43:27 +0545

    created an serializer

--- Social/user/serializer.py
+++ Social/user/serializer.py
@@ -9,4 +9,27 @@ class UserSerializer(serializers.ModelSerializer):
         fields=['id','username','passeord']
         extra_kwargs={'password':{'write_only':True}}
 
+    def create(self,validated_data):
+        user=User.objects.create_user(**validated_data)
+        return user
+    
 
+class  PostSerializer(serializers.ModelSerializer):
+    author=serializers.ReadOnlyField(source='author.username')
+    likes_count=serializers.SerializerMethodField()
+
+    class Meta:
+        model=Post
+        fields=['id','author','content','likes_count','created_at']
+
+    def get_likes_count(self,obj):
+        return obj.likes_count()
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author=serializers.ReadOnlyField(source='author.username')
+
+
+    class Meta:
+        model=Comment
+        fields=['id','post','author','content','created_at']
